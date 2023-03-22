e1751387f6d78fe4aca8265722cbe0d04b216366
SergeantAnt <anthonysargent124@yahoo.com>
2023-03-22 12:02:57 -0600

    Got the template working working!

---
+++ fuel/app/views/TemplateGroup.php
@@ -0,0 +1,17 @@
+<head>
+    <title><?php $title; ?> </title>
+    <meta charset="utf-8"></meta>
+    <meta name="author" content="Anthony"></meta>
+    <meta name="description" content="Fuel Template"></meta>
+
+
+</head>
+<body>
+<header>
+    <h1><?php echo $pg ?></h1>
+</header>
+
+<main>
+<?php echo $content;?>
+</main>
+</body>

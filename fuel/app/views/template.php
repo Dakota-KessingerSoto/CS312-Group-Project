2a50360e87af993d3075f879005290907520e060
Dakota <dkess@colostate.edu>
2023-03-26 14:39:58 -0600

    Updating home page view and css

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="keywords"  content="HTML,CSS,JavaScript,PHP">
-		<?php echo Asset::css($style); ?>
-	</head>
-	<body>
-		<header>
-            <section class="title"><h1><?php echo $heading; ?></h1></section>
-            <section class="navbar">
-                <nav>
-                    <ul>
-						<li><a href="./index" >  Home  </a></li>
-						<li><a href="./about" >  About  </a></li>
-						<li><a href="./table" >  Table  </a></li>
-                    </ul>
-                </nav>
-            </section>
-        </header>
-		<?php echo $content; ?>
-	</body>
-	<footer>
-            <p>Authors: Kevin Truong            <a href="truongak@colostate.edu">truongak@colostate.edu</a></p>
-            <p>Authors: Dakota Kessinger-Sott   <a href="email@colostate.edu">email@colostate.edu</a></p>
-            <p>Authors: Anthony Sargent         <a href="antsarge@colostate.edu">antsarge@colostate.edu</a></p>
-            <p>Authors: John Mcfall             <a href="email@colostate.edu">email@colostate.edu</a></p>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="keywords" content="HTML,CSS,JavaScript,PHP">
+    <?php echo Asset::css($style); ?>
+</head>
+<body>
+    <header>
+        <section class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="./index" >  Home  </a></li>
+                    <li><a href="./about" >  About  </a></li>
+                    <li><a href="./table" >  Table  </a></li>
+                </ul>
+            </nav>
+        </section>
+        <section class="heading">
+            <h1><?php echo $heading; ?></h1>
+        </section>
+    </header>
+    <main>
+        <?php echo $content; ?>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> ColorPalettePro. All rights reserved.</p>
+        <p>Contact us: support@colorpalettepro.com</p>
     </footer>
 </html>
